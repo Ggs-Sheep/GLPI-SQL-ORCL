6dc3125d4167389928142e846f6fbc86944f1c20
clarence claux <claux59.c@gmail.com>
2023-04-10 22:32:23 +0200

    update

--- create_table.sql
+++ create_table.sql
@@ -1,101 +1,96 @@
 -- Création de la table Utilisateurs
-alter session set "_ORACLE_SCRIPT"=true;
-CREATE TABLE Utilisateurs (
-    id_utilisateur NUMBER(10) PRIMARY KEY,
-    nom VARCHAR2(50) NOT NULL,
-    prenom VARCHAR2(50) NOT NULL,
-    email VARCHAR2(100) UNIQUE,
+alter session set "_ORACLE_SCRIPT"= true;
+CREATE TABLE Utilisateur
+(
+    id_utilisateur   NUMBER(10) PRIMARY KEY,
+    nom              VARCHAR2(50) NOT NULL,
+    prenom           VARCHAR2(50) NOT NULL,
+    email            VARCHAR2(100) UNIQUE,
     type_utilisateur VARCHAR2(50)
 );
-ALTER TABLE Utilisateurs
-ADD CONSTRAINT email_check CHECK (REGEXP_LIKE(email, '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'));
-ALTER TABLE Utilisateurs
-ADD CONSTRAINT chk_type_utilisateur CHECK (type_utilisateur IN ('technicien', 'utilisateur'));
+ALTER TABLE Utilisateur
+    ADD CONSTRAINT email_check CHECK (REGEXP_LIKE(email, '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'));
+ALTER TABLE Utilisateur
+    ADD CONSTRAINT chk_type_utilisateur CHECK (type_utilisateur IN ('technicien', 'utilisateur'));
 
-
--- Création de la table Projet
-CREATE TABLE Projet (
-    id_projet NUMBER(10) PRIMARY KEY,
-    nom_projet VARCHAR2(50) NOT NULL,
-    description VARCHAR2(500),
-    date_creation DATE,
-    date_modification DATE,
-    etat_projet VARCHAR2(50),
-    site VARCHAR2(50)
+CREATE TABLE Departement
+(
+    id_departement  NUMBER(10) PRIMARY KEY,
+    nom_departement VARCHAR2(50) NOT NULL
 );
-CREATE INDEX idx_projet_nom_projet
-ON Projet (nom_projet);
 
 
 -- Création de la table Matériel
-CREATE TABLE Materiel (
-    id_materiel NUMBER(10) PRIMARY KEY,
+CREATE TABLE Materiel
+(
+    id_materiel   NUMBER(10) PRIMARY KEY,
     type_materiel VARCHAR2(50) NOT NULL,
-    date_achat DATE,
+    date_achat    DATE,
     date_garantie DATE,
-    date_fin_vie DATE,
+    date_fin_vie  DATE,
     etat_materiel VARCHAR2(50)
 );
+ALTER TABLE Materiel
+    ADD CONSTRAINT chk_type_materiel_type CHECK (type_materiel IN
+                                                 ('ordinateur fixe', 'ordinateur portable', 'serveur', 'scanner',
+                                                  'imprimante', 'telephone'));
+ALTER TABLE Materiel
+    ADD CONSTRAINT chk_type_materiel_etat CHECK (etat_materiel IN
+                                                 ('neuf', 'bon etat', 'etat moyen', 'mauvais etat', 'en reparation',
+                                                  'hors service'));
 CREATE INDEX idx_materiel_etat_materiel
-ON Materiel (etat_materiel);
+    ON Materiel (etat_materiel);
 
 
 -- Création de la table Logiciels
-CREATE TABLE Logiciels (
-    id_logiciel NUMBER(10) PRIMARY KEY,
-    nom_logiciel VARCHAR2(50) NOT NULL,
-    type_logiciel VARCHAR2(50),
-    version VARCHAR2(50),
-    date_achat DATE,
+CREATE TABLE Logiciel
+(
+    id_logiciel      NUMBER(10) PRIMARY KEY,
+    nom_logiciel     VARCHAR2(50) NOT NULL,
+    type_logiciel    VARCHAR2(50),
+    version          VARCHAR2(50),
+    date_achat       DATE,
     date_fin_support DATE
 );
 CREATE INDEX idx_logiciels_nom_logiciel
-ON Logiciels (nom_logiciel);
+    ON Logiciel (nom_logiciel);
 
 
 -- Création de la table Tickets
-CREATE TABLE Tickets (
-    id_ticket NUMBER(10) PRIMARY KEY,
-    titre VARCHAR2(50) NOT NULL,
-    description VARCHAR2(500),
-    date_creation DATE,
+CREATE TABLE Ticket
+(
+    id_ticket         NUMBER(10) PRIMARY KEY,
+    titre             VARCHAR2(50) NOT NULL,
+    description       VARCHAR2(500),
+    date_creation     DATE,
     date_modification DATE,
-    date_cloture DATE,
-    etat_ticket VARCHAR2(50),
-    site VARCHAR2(50),
-    projet_id NUMBER(10),
-    FOREIGN KEY (projet_id) REFERENCES Projet(id_projet)
+    date_cloture      DATE,
+    etat_ticket       VARCHAR2(50),
+    site              VARCHAR2(50)
 );
 CREATE INDEX idx_tickets_titre
-ON Tickets (titre);
+    ON Ticket (titre);
 
-CREATE TABLE Utilisateurs_Tickets (
-    utilisateur_id NUMBER(10),
-    ticket_id NUMBER(10),
-    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(id_utilisateur),
-    FOREIGN KEY (ticket_id) REFERENCES Tickets(id_ticket)
+CREATE TABLE Utilisateur_Ticket
+(
+    id_utilisateur NUMBER(10),
+    ticket_id      NUMBER(10),
+    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur (id_utilisateur),
+    FOREIGN KEY (ticket_id) REFERENCES Ticket (id_ticket)
 );
 
-CREATE TABLE Utilisateurs_Materiel (
+CREATE TABLE Utilisateur_Materiel
+(
     id_utilisateur NUMBER(10) NOT NULL,
-    id_materiel NUMBER(10) NOT NULL,
-    CONSTRAINT pk_utilisateurs_materiel PRIMARY KEY (id_utilisateur, id_materiel),
-    CONSTRAINT fk_utilisateurs_materiel_utilisateurs FOREIGN KEY (id_utilisateur) REFERENCES Utilisateurs (id_utilisateur),
-    CONSTRAINT fk_utilisateurs_materiel_materiel FOREIGN KEY (id_materiel) REFERENCES Materiel (id_materiel)
+    id_materiel    NUMBER(10) NOT NULL,
+    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur (id_utilisateur),
+    FOREIGN KEY (id_materiel) REFERENCES Materiel (id_materiel)
 );
 
-CREATE TABLE Utilisateurs_Logiciels (
+CREATE TABLE Utilisateur_Logiciel
+(
     id_utilisateur NUMBER(10) NOT NULL,
-    id_logiciel NUMBER(10) NOT NULL,
-    CONSTRAINT pk_utilisateurs_logiciels PRIMARY KEY (id_utilisateur, id_logiciel),
-    CONSTRAINT fk_utilisateurs_logiciels_utilisateurs FOREIGN KEY (id_utilisateur) REFERENCES Utilisateurs (id_utilisateur),
-    CONSTRAINT fk_utilisateurs_logiciels_logiciels FOREIGN KEY (id_logiciel) REFERENCES Logiciels (id_logiciel)
-);
-
-CREATE TABLE Materiel_Logiciels (
-    id_materiel NUMBER(10) NOT NULL,
-    id_logiciel NUMBER(10) NOT NULL,
-    CONSTRAINT pk_materiel_logiciels PRIMARY KEY (id_materiel, id_logiciel),
-    CONSTRAINT fk_materiel_logiciels_materiel FOREIGN KEY (id_materiel) REFERENCES Materiel (id_materiel),
-    CONSTRAINT fk_materiel_logiciels_logiciels FOREIGN KEY (id_logiciel) REFERENCES Logiciels (id_logiciel)
+    id_logiciel    NUMBER(10) NOT NULL,
+    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur (id_utilisateur),
+    FOREIGN KEY (id_logiciel) REFERENCES Logiciel (id_logiciel)
 );
---
+++ vues.sql
@@ -0,0 +1,9 @@
+CREATE VIEW materiels_hors_garantie AS
+SELECT *
+FROM MATERIEL
+WHERE date_garantie < ADD_MONTHS(SYSDATE, 2);
+
+CREATE VIEW vue_materiel_hors_service AS
+SELECT *
+FROM MATERIEL
+WHERE etat_materiel = 'hors service';
