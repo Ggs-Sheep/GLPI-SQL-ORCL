33a11556868924ee3ab7f96a783ba1f1acc2c98a
clarence claux <claux59.c@gmail.com>
2023-04-13 13:23:50 +0200

    adding views

--- vues.sql
+++ vues.sql
@@ -7,3 +7,14 @@ CREATE VIEW vue_materiel_hors_service AS
 SELECT *
 FROM MATERIEL
 WHERE etat_materiel = 'hors service';
+
+CREATE VIEW vue_utilisateurs_techniciens AS
+SELECT *
+FROM Utilisateur
+WHERE type_utilisateur = 'technicien';
+
+CREATE VIEW Vue_des_tickets_ouverts AS
+SELECT id_ticket, titre, description, date_creation, date_modification, etat_ticket, site
+FROM Ticket
+WHERE etat_ticket = 'ouvert';
+
